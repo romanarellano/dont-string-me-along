c99a2383e07e2361f6de604566b126dd85318dac
Brandon Baris <brandonbaris@gmail.com>
2015-01-27 19:44:02 -1000

    some blahblah

--- Dont-String.js
+++ Dont-String.js
@@ -1,18 +1,16 @@
-
-
-
 function missingNum(str){
 
+  var excludedNum=[];
 
-var excludedNum=[];
-for(var i=0;i<10;i++){
-if(str.indexOf(i)===-1){
-    excludedNum.push(i);
+  for(var i=0;i<10;i++){
+    if(str.indexOf(i)===-1){
+        excludedNum.push(i);
+  }
+
+    }
+  return excludedNum;
 }
 
-}
-return excludedNum;
-}
 
 
 
